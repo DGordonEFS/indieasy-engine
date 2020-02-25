a30d9971e96c3a93882a398667280b857ab0e413
= <dale.efs@gmail.com>
2020-02-25 13:32:46 -0500

    v0.0.13

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,15 @@
 import { createStore as createBaseStore, combineReducers } from 'redux';
 
 const reducers = {};
+let store;
 
 export const addReducer = (id, reducer) => {
 	reducers[id] = reducer;
 };
 
 export const createStore = () => {
-	window.store = createBaseStore(combineReducers(reducers));
-	return window.store;
+	store = createBaseStore(combineReducers(reducers));
+	return store;
 };
 
-export const getStore = () => window.store;
+export const getStore = () => store;
