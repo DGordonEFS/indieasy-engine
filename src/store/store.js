5ab2939f45c2e863118bb587eac7eab7ca06c8c9
= <dale.efs@gmail.com>
2020-02-25 13:11:57 -0500

    window.store

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,14 @@
 import { createStore as createBaseStore, combineReducers } from 'redux';
 
-let store;
 const reducers = {};
 
 export const addReducer = (id, reducer) => {
-  reducers[id] = reducer;
+	reducers[id] = reducer;
 };
 
 export const createStore = () => {
-  store = createBaseStore(combineReducers(reducers));
-  return store;
+	window.store = createBaseStore(combineReducers(reducers));
+	return window.store;
 };
 
-export const getStore = () => store;
+export const getStore = () => window.store;
